9a1408ad3f3eb59853743122f1a9b8f1ab2b4ada
Kamil Konopka <kamil.konopka@thomsonreuters.com>
2018-12-07 09:16:24 +0100

    created basic components and auxilliary higher order component to wrap multiple markup elements

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Auxilliary from '../../hoc/Auxilliary';
+
+const layout = (props) => (
+    <Auxilliary>
+        <div>Toolbar, SideDrawe, Backdrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Auxilliary>
+
+);
+
+export default layout;
